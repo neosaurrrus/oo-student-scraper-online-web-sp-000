a50095bf0b0b83d63b66d6a0e0cf8d7dee0b38e4
Lukie Kang <lukiek+f@gmail.com>
2019-08-08 16:08:14 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,26 @@ require 'pry'
 
 class Scraper
 
+  def self.get_url(index_url)
+    Nokogiri::HTML(open(index_url))
+  end
+
   def self.scrape_index_page(index_url)
-    
+    scraped_index = []
+    scraped_cards = get_url(index_url).css('.student-card')
+    card_array = scraped_cards.css('.student-card').map do |card|
+      card.content
+    end
+
+    binding.pry
+    #name = something from scraped cards
+    #location - something from scraped cards
+    #get name, location, profile_url
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
